0296d39c75d8b2b0559dbecd6684da61768b344a
sbghule212 <101106017+sbghule212@users.noreply.github.com>
2025-06-21 09:30:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ function calculateForward() {
   const onlinePrice = Math.round(dpPrice * 0.9);
   const retailPrice = offlinePrice + 100;
   const twentypPrice = Math.round(dpPrice * 0.8);
-  const profit = (profit / (100 + profit)) * 100;
+  const discount = (profit / (100 + profit)) * 100;
 
   // document.getElementById("offlinePrice").textContent = offlinePrice;
   // document.getElementById("onlinePrice").textContent = onlinePrice;
   // document.getElementById("retailPrice").textContent = retailPrice;
   document.getElementById("twentypPrice").textContent = twentypPrice;
-  document.getElementById("profit").textContent = profit;
+  document.getElementById("profit").textContent = discount;
 }
 
 function calculateReverse() {
