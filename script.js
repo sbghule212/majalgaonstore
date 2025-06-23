595f9e5df8254fb8e28ba5f7396caace2394716f
sbghule212 <101106017+sbghule212@users.noreply.github.com>
2025-06-23 15:29:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function mround(value, multiple) {
 
   const base = dpPrice * 0.75;
   const purchasePrice = mround(dpPrice / 1.5, 5);
-  const msp = mround(taxablePrice * 1.15, 5);
+  const msp = mround(purchasePrice * 1.15, 5);
   const homeShopee = mround(msp * 1.10, 5);
   const ltCustomer = mround(homeShopee * 1.10, 5);
 
